7d81db390ff265bde5a6f6897f73b7c44240ffdc
Cook0117 <ryancook011703@gmail.com>
2024-12-12 10:31:00 -0500

    vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,19 @@
 // vue.config.js
 module.exports = {
-    publicPath: '/',
-    outputDir: 'dist',
-    productionSourceMap: false,
-    devServer: {
-        open: true,
-        port: 8080,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3000',
-                changeOrigin: true,
-            },
-        },
-    },
-    css: {
-        sourceMap: true,
-    },
+  publicPath: '/',
+  outputDir: 'dist',
+  productionSourceMap: false,
+  devServer: {
+      open: true,
+      port: 8080,
+      proxy: {
+          '/api': {
+              target: 'http://localhost:3000',
+              changeOrigin: true,
+          },
+      },
+  },
+  css: {
+      sourceMap: true,
+  },
 };
